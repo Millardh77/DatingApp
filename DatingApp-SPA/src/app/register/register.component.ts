d9548c4e84d75081ecf2d2683b671ebf714228cb
Millard Harris <millard@freemountaininc.com>
2020-07-05 12:34:37 -0700

    End of Section 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +11,7 @@ export class RegisterComponent implements OnInit {
   model: any = {};
 
   @Output() cancelRegister = new EventEmitter();
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
@@ -20,9 +20,9 @@ export class RegisterComponent implements OnInit {
   // tslint:disable-next-line: typedef
   register() {
   this.authService.register(this.model).subscribe(() => {
-    console.log('registration successful');
+    this.alertify.success('registration successful');
   }, error => {
-    console.log(error);
+    this.alertify.error(error);
   });
   }
 
