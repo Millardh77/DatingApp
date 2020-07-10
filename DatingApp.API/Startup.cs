e9cbbbba709c8e70a75709f69b545d17feac8568
Millard Harris <millard@freemountaininc.com>
2020-07-10 16:14:04 -0700

    End of Section 13

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -46,6 +46,7 @@ namespace DatingApp.API
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
+           
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => 
                 {
@@ -57,6 +58,8 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+
+             services.AddScoped<LogUserActivity>();
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
